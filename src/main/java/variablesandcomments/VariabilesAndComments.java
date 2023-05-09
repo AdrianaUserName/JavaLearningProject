769783ed7a2b9c65a9a92cbb090f29c7484593cd
AdrianaUserName <adrianapraporscic@gmail.com>
2023-05-09 12:52:57 +0300

    Added the Variables and Comments Task

---
+++ src/main/java/variablesandcomments/VariabilesAndComments.java
@@ -0,0 +1,48 @@
+package variablesandcomments;
+
+public class VariabilesAndComments {
+    public static void main(String[] args) {
+
+//Declaram toate tipurile de date primitive
+
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte loverLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+//Initializarea variabilelor declarate
+
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        firstCharFromAscii = 'a';
+        lastCharFromAscii = 'z';
+        loverLimitOfByteDataType = -128;
+        upperLimitOfByteDataType = 127;
+        lowerLimitOfShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+        lowerLimitOfIntDataType = -2147483648;
+        upperLimitOfIntDataType = 2147483647;
+        lowerLimitOfLongDataType = -9223372036854775808;
+        upperLimitOfLongDataType = 9223372036854775807;
+        lowerLimitOfFloatDataType = -3.4E + 38f;
+        upperLimitOfFloatDataType = 3.4E+38f;
+        lowerLimitOfDoubleDataType = -1.7E+308D;
+        upperLimitOfDoubleDataType = 1.7E+305;
+
+
+    }
+
+
+}
