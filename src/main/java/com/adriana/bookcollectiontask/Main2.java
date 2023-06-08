ba8be590bc1113ccd302dbcc52857fd12ecd68ab
AdrianaUserName <adrianapraporscic@gmail.com>
2023-06-08 12:03:12 +0300

    Added the implementation of a library system by using the collections

---
+++ src/main/java/com/adriana/bookcollectiontask/Main2.java
@@ -0,0 +1,15 @@
+package com.adriana.bookcollectiontask;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int[] tablouDeDateInt = {1,2,3,8,4,0,5,6};
+
+        System.out.println(IntNumberArrayService.getTheMinValueOfTheArray(tablouDeDateInt));
+        System.out.println(IntNumberArrayService.getTheMinValueIndexOfTheArray(tablouDeDateInt));
+
+
+        int a = 5;
+        int b = 8;
+
+    }
+}
